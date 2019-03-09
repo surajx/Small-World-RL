6b19a833584c06829fb78cec9ed78e4ed4090df4
Suraj Narayanan S <surajx@gmail.com>
2019-03-09 13:05:53 +0530

    autopep8 formatting

--- src/Runner.py
+++ src/Runner.py
@@ -9,20 +9,23 @@ from Environment import *
 import Agents
 import Environments
 
-def load_env( env_type ):
+
+def load_env(env_type):
     """Try to construct an environment"""
-    mod = __import__("Environments.%s"%(env_type), fromlist=[Environments])
-    assert( hasattr(mod, env_type) )
-    envClass = getattr( mod, env_type )
+    mod = __import__("Environments.%s" % (env_type), fromlist=[Environments])
+    assert(hasattr(mod, env_type))
+    envClass = getattr(mod, env_type)
     return envClass
 
-def load_agent( agent_type ):
+
+def load_agent(agent_type):
     """Try to construct an agent"""
 
-    mod = __import__("Agents.%s"%(agent_type), fromlist=[Agents])
-    assert( hasattr(mod, agent_type) )
-    agentClass = getattr( mod, agent_type )
+    mod = __import__("Agents.%s" % (agent_type), fromlist=[Agents])
+    assert(hasattr(mod, agent_type))
+    agentClass = getattr(mod, agent_type)
     return agentClass
+
 
 def run(env, agent, episodes):
     """ Simulate some episodes of running """
@@ -38,22 +41,19 @@ def run(env, agent, episodes):
         state, reward, episode_ended = env.react(action)
 
         # Add rewards to ret
-        if isinstance( action, Option ):
+        if isinstance(action, Option):
             # If this was an option, then multiple rewards would have been
             # returned.
-            ret += sum( reward )
-            epochs += len( state ) - 1
+            ret += sum(reward)
+            epochs += len(state) - 1
         else:
             ret += reward
             epochs += 1
 
         if episode_ended:
-            episodic_return.append( ret )
-            episodic_epochs.append( epochs )
+            episodic_return.append(ret)
+            episodic_epochs.append(epochs)
             epochs = 0
 
-
-
     # Chop off any extras
-    return ret[ : epochs ]
-
+    return ret[: epochs]
